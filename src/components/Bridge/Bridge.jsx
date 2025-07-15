2425dbfe6f4435a812b74f7dbe18db864e91ad27
William Sebastian <okeefesebastian91@gmail.com>
2025-07-15 16:46:53 +0900

    styled the bridge component

--- src/components/Bridge/Bridge.jsx
+++ src/components/Bridge/Bridge.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Bridge.css";
 
-function Bridge() {
-  return (
-    <div>
+function Bridge( {isFirst, isLast, position } ) {
+  let className = "bridge";
+  if (isFirst) className += " bridge-first";
+  else if (isLast) className += " bridge-last";
+  else className += ` bridge-${position}`;
 
+  return (
+    <div className={className}>
+      <span> { position + 1 } </span>
     </div>
   )
 }
