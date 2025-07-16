e44d4dee5d022e8da1a3e68bfad5e7fa4f104aa4
William Sebastian <okeefesebastian91@gmail.com>
2025-07-16 11:18:55 +0900

    applied styling to koto body

--- src/components/Koto/Koto.jsx
+++ src/components/Koto/Koto.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import StringGroup from "../StringGroup/StringGroup";
 import "./Koto.css";
+import BridgeGroup from "../BridgeGroup/BridgeGroup";
 
 function Koto() {
   return (
     <div className="koto">
       <StringGroup />
+      <BridgeGroup />
     </div>
   )
 }
